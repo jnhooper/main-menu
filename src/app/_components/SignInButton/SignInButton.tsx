a2b2fc4ffe4e2713cc3b2c0794b96299906d1936
John Hooper <nisse038@gmail.com>
2025-12-30 12:38:34 -0600

    redirect on login

---
+++ src/app/_components/SignInButton/SignInButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { signIn, signOut } from "next-auth/react";
+
+export const SignInButton = ({ signedIn }: { signedIn: boolean }) => {
+  return signedIn
+    ? (
+      <button
+        type="button"
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        onClick={() => signOut()}
+      >
+        Sign out
+      </button>
+    )
+    : (
+      <button
+        type="button"
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        onClick={
+          // Change the callbackUrl to your desired redirect
+          () => signIn(undefined, { callbackUrl: "/myMenu" })
+        }
+      >
+        Sign in
+      </button>
+    );
+};
--- src/app/hh/page.tsx
+++ src/app/hh/page.tsx
@@ -1,21 +1,22 @@
 import { auth } from "~/server/auth";
-import {  HydrateClient, api } from "~/trpc/server";
+import { api, HydrateClient } from "~/trpc/server";
 import { CreateHousehold } from "../_components/households/CreateHousehold";
 import { MyHouseholdList } from "./MyHouseholdList";
-export default async function HouseholdPage(){
-
+export default async function HouseholdPage() {
   const session = await auth();
-  const myHouseholds = session?.user ? 
-    await api.household.getMyHouseholds(): []
-
+  const myHouseholds = session?.user
+    ? await api.household.getMyHouseholds()
+    : [];
 
   return (
     <HydrateClient>
-      <div className="w-full max-w-xs">
-        <h1>Households</h1>
-        <MyHouseholdList initialHouseholds={myHouseholds}/>
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <h1 className="text-xl">
+          Households
+        </h1>
+        <MyHouseholdList initialHouseholds={myHouseholds} />
         <CreateHousehold />
       </div>
     </HydrateClient>
-  )
+  );
 }
