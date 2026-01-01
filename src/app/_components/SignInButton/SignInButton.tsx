5c72cbfbc42b7124096fe07d3ed9d1cdfc5820fc
John Hooper <nisse038@gmail.com>
2026-01-01 00:20:30 -0600

    signout redirect to home screen

--- src/app/_components/SignInButton/SignInButton.tsx
+++ src/app/_components/SignInButton/SignInButton.tsx
@@ -7,7 +7,7 @@ export const SignInButton = ({ signedIn }: { signedIn: boolean }) => {
       <button
         type="button"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-        onClick={() => signOut()}
+        onClick={() => signOut({ callbackUrl: "/", redirect: true })}
       >
         Sign out
       </button>
